a5e39ebf3e3280424bf6cf478e4cf8878ffa58d5
Lucas Santos <lucas.santos.silva2011@gmail.com>
2024-05-19 23:03:43 -0300

    Simplify year type

--- src/usecases/data_production/dtos.py
+++ src/usecases/data_production/dtos.py
@@ -5,6 +5,6 @@ class RequestProduction(BaseModel):
     """
     Classe responsável por definir os parâmetros de request da rota get_production
     """
-    year: Union[Union[List[int], int], Union[None, str]] = Field(title='Ano', description='Ano dos dados a serem retornados', default=1970)
+    year: Union[List[int], int, None, str] = Field(title='Ano', description='Ano dos dados a serem retornados', default=1970)
     year_as_unique_column: bool = Field(title='Tipo Ano', description='Se informado True, o ano será retornado como uma coluna unificada', default=False)
     save_dataset_localy: bool = Field(title='Salvar dataset', description='Indica se deve salvar o dataset localmente na API', default=False)
